ec8f2383f89ecdde93fba6bff9e97a85ee01c0c6
kendrickkilat <kendrick.kilat@mllrdev.com>
2024-05-21 17:27:30 +0800

    ui: make messages easier tor read

--- src/components/talk.vue
+++ src/components/talk.vue
@@ -3,17 +3,17 @@
     <div :class="{ 'flex justify-end': message.sender === USER.OPENAI }">
       <div
         v-if="message.sender === USER.OPENAI"
-        class="bg-green-400 text-white max-w-3xl p-3 mb-3 rounded-xl mr-4"
+        class="bg-green-600 text-white max-w-3xl p-3 mb-3 rounded-xl mr-4"
       >
         <label class="mb-2 font-bold">USER</label>
         <p v-html="message.content" />
       </div>
       <div
         v-else
-        class="bg-purple-400 text-white max-w-3xl p-3 mb-3 rounded-xl ml-4"
+        class="bg-purple-700 text-white max-w-3xl p-3 mb-3 rounded-xl ml-4"
       >
         <label class="mb-2 font-bold">GEMINI</label>
-          <p v-html="message.content"></p>
+          <p v-html="message.content" markdown="1"></p>
       </div>
     </div>
   </div>
